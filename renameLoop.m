e73c13ca79a66c78155fc78b6b690b3ca1e2ea0b
thomasbrockmeier <thomas.brockmeier@gmail.com>
2014-11-24 14:15:30 +0100

    leave out changed file names
    by starting at dirNames(15)
    
    (dirNames(14) and before already having had their file names changed)

--- renameLoop.m
+++ renameLoop.m
@@ -1,10 +1,10 @@
-function renameLoop( dataDir )              % 'E:\Thomas\Data'
+function renameLoop( dataDir )                  % 'E:\Thomas\Data'
 % Loop conditionRename.m to change all Net Station output file names to NBT
 % format. See help conditionRename for information.
 
 dirNames        = dir(dataDir);
 
-for i = 1:length(dirNames)
+for i = 1:length(dirNames(15:end))              % dirNames(14) = S0011
     try
         if dirNames(i).name(1:6) == 'MP14.S'    % Check for data folder
             conditionRename([ dataDir '\' dirNames(i).name ])
