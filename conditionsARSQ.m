1a2e2403cb34b7467c2c20eafa51940580adb8fd
thomasbrockmeier <thomas.brockmeier@gmail.com>
2015-01-22 13:38:28 +0100

    gfdgfdgfdgdfgdfd

---
+++ conditionsARSQ.m
@@ -0,0 +1,178 @@
+function [ conditions, f ] = conditionsARSQ()
+% Retrieves subject responses per condition
+%
+% E.g., targetDir = 'I:\Music_project\all subjects - DFA'; pwd
+% Requires nbt_NBTRunAnalysisPleasureRatings_DFAmusic to have been run
+
+%% Call vars
+files = dir([pwd '\*_analysis.mat']);
+nSubjects = floor(length(files) / 15);      % n Files / 15 conditions                                           % unused subjects
+
+conditionsList = {'BH1', 'BH2', 'BR1', 'BR2', 'CH1', 'CH1R', 'CH2', ...
+    'GR1', 'GR2', 'HD1', 'HD2', 'HD2R', 'MZ1', 'MZ2', 'MZ2R'};
+conditions = cell(5, 15);
+
+pleasure = zeros(nSubjects, 1);
+familiar = zeros(nSubjects, 1);
+concetration = zeros(nSubjects, 1);
+eyesOpen = zeros(nSubjects, 1);
+
+%% Create cell array containing subject responses
+for i = 1:length(conditionsList)
+    for ii = 1:nSubjects
+        % Load biomarkers
+        fileNameCond = files(i + 15 * (ii - 1)).name;
+        bioMarkers = load([pwd '\' fileNameCond]);
+        responses = bioMarkers.rsq.Answers;
+        
+        % Retrieve subject's responses
+        pleasure(ii, 1) = responses(1);
+        familiar(ii, 1) = responses(2);
+        concetration(ii, 1) = responses(3);
+        eyesOpen(ii, 1) = responses(4);
+        rhythmDFA = responses(5);
+        pitchDFA = responses(6);
+        loudnessDFA = responses(7);
+        averageDFA = responses(8);
+    end 
+    
+    % Fill cell array
+    conditions{1, i} = conditionsList{i};
+    conditions{2, i} = pleasure;
+    conditions{3, i} = familiar;
+    conditions{4, i} = concetration;    
+    conditions{5, i} = eyesOpen;
+    conditions{6, i} = rhythmDFA;
+    conditions{7, i} = pitchDFA;
+    conditions{8, i} = loudnessDFA;
+    conditions{9, i} = averageDFA;
+end
+
+% Get response frequencies
+conditionsTemp = conditions;
+conditionsTemp(1, :) = [];
+[~, freqPL] = cellfun(@mode, conditionsTemp);
+
+%% Create histograms of subject responses per condition
+for j = 4:-1:1
+    figure
+    
+    for jj = 1:length(conditionsList)
+        subplot(3, 5, jj);
+        
+        if j == 4
+            [ f, x ] = hist(conditions{j + 1, jj}, 0:1);
+            h = bar(x, f);
+            axis([-1 2 0 max(freqPL(j, :)) + 1]);
+        else
+            [ f, x ] = hist(conditions{j + 1, jj}, 0:6);
+            h = bar(x, f);
+            axis([-1 7 0 max(freqPL(j, :)) + 1]);
+        end
+        
+        title(conditionsList{jj});
+        xlabel('Response');
+        ylabel('Frequency');
+        set(h, 'FaceColor', [0 0.5 0.5], 'EdgeColor', 'none');
+    end
+    
+    if j == 4
+        suptitle(sprintf('Subjects with Eyes Closed (0)/Open (1) per Condition, N = %d', nSubjects));
+    elseif j == 3
+        suptitle(sprintf('Concentration Ratings per Condition, N = %d', nSubjects));
+    elseif j == 2
+        suptitle(sprintf('Familiarity Ratings per Condition, N = %d', nSubjects));
+    elseif j == 1
+        suptitle(sprintf('Pleasurability Ratings per Condition, N = %d', nSubjects));
+    end
+end
+
+%% Create histograms of subject responses per DFA condition
+for k = 1:4                                        % Responses
+    if k == 1
+        DFAcond = 'Pleasurability Ratings';
+        condRow = 2;
+    elseif k == 2
+        DFAcond = 'Familiarity Ratings';
+        condRow = 3;
+    elseif k == 3
+        DFAcond = 'Concentration Ratings';
+        condRow = 4;
+    elseif k == 4
+        DFAcond = 'Subjects with Eyes Closed (0)/Open (1)';
+        condRow = 5;
+    end
+    
+    figure
+    sPlotID = [1 5 9 2 6 10 3 7 11 4 8 12];
+    sPlotct = 0;
+    for kk = 1:4                                   % R/P/L/Avg
+        if kk == 1
+            DFArow = 6;
+            DFAtype = 'Rhythm ';
+        elseif kk == 2
+            DFArow = 7;
+            DFAtype = 'Pitch ';
+        elseif kk == 3
+            DFArow = 8;
+            DFAtype = 'Loudness ';
+        elseif kk == 4
+            DFArow = 9;
+            DFAtype = 'RPL DFA_a_v_g ';
+        end
+        
+        for kkk = 1:3                              % DFA bins
+            if kkk == 1
+                minTargetDFA = 0;
+                maxTargetDFA = 0.65;
+                DFAtitle = 'DFA < 0.65';
+            elseif kkk == 2
+                minTargetDFA = 0.65;
+                maxTargetDFA = 0.85;
+                DFAtitle = '0.65 < DFA < 0.85';
+            elseif kkk == 3
+                minTargetDFA = 0.85;
+                maxTargetDFA = 2;
+                DFAtitle = 'DFA > 0.85';
+            end
+            
+            c = 0;
+            targetStim = [];
+            for kkkk = 1:length(conditionsList)    % Stimuli
+                if conditions{DFArow, kkkk} > minTargetDFA && ...
+                        conditions{DFArow, kkkk} < maxTargetDFA
+                    c = c + 1;
+                    targetStim(c) = kkkk;
+                end
+            end
+
+            concatVec = [];
+            for kkkkk = 1:length(targetStim)       % Concatenate vectors...
+                targetVec = conditions{condRow, targetStim(kkkkk)};
+                targetVec = targetVec';
+                concatVec = [concatVec targetVec];
+            end
+            
+            sPlotct = sPlotct + 1;
+            nDFACond = length(concatVec) / length(conditions{2, 1});
+            subplot(3, 4, sPlotID(sPlotct))        % ... and plot.
+            [ f, x ] = hist(concatVec, 0:6);
+            disp(f/sum(f))
+            h = bar(x, f/sum(f) * 100);
+            title(sprintf('%s\n%s%s, N_S_t_i_m_u_l_i = %d', DFAcond, DFAtype, DFAtitle, nDFACond));
+            xlabel('Response');
+            ylabel('Frequency (%)');
+            set(h, 'FaceColor', [0 0.5 0.5], 'EdgeColor', 'none');
+            if k ~= 4
+                axis([-1 7 0 50]);
+            else
+                axis([-1 1.9999 0 100]);
+            end
+            
+            if sPlotct == 12
+                suptitle([DFAcond ' Per DFA Category']);
+            end
+        end
+    end
+end
+end
---
+++ nbt_NBTRunAnalysisAHN_DFA.m
@@ -0,0 +1,188 @@
+% Copyright (C) 2012  Neuronal Oscillations and Cognition group, Department of Integrative Neurophysiology, Center for Neurogenomics and Cognitive Research, Neuroscience Campus Amsterdam, VU University Amsterdam.
+%
+% Part of the Neurophysiological Biomarker Toolbox (NBT)
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% See Readme.txt for additional copyright information.
+%
+
+% ChangeLog - see version control log for details
+% <date> - Version <#> - <text>
+
+function nbt_NBTRunAnalysisAll(varargin)
+script = NBTwrapper();
+nbt_NBTcompute(script,'RawSignal',pwd,pwd)
+end
+
+
+function NBTfunction_handle = NBTwrapper()
+
+delta_hp = 1;
+delta_lp = 4;
+delta_fo = 2;
+
+theta_hp = 4;
+theta_lp = 8;
+theta_fo = 0.5;
+
+alpha_hp = 8;
+alpha_lp = 13;
+alpha_fo = 0.26;
+
+beta_hp = 13;
+beta_lp = 30;
+beta_fo = 0.16;
+
+gamma_hp = 30;
+gamma_lp = 45;
+gamma_fo = 2/30;
+
+highGamma_hp = 55;
+highGamma_lp = 125;
+highGamma_fo = 2/55;
+
+noharmonicsGamma_hp = 60;
+noharmonicsGamma_lp = 90;
+noharmonicsGamma_fo = 2/60;
+
+
+DFAshortFit = 1;
+DFAlongFit = 20;
+DFAshortCalc = 0.1;
+DFAlongCalc = 80;
+DFA_Overlap = 0.5;
+DFA_Plot = 0;
+ChannelToPlot = 1;
+
+    function NBTscript(Signal, SignalInfo, SaveDir)
+       % [Signal, SignalInfo] = nbt_EEGLABwrp(@pop_resample, Signal, SignalInfo, [], 0, 200);
+      %  SignalInfo.converted_sample_frequency = 200;
+        
+%         Hjorthpara = nbt_doHjorthPara(Signal, SignalInfo,0,0);
+%         nbt_SaveClearObject('Hjorthpara', SignalInfo, SaveDir)
+%         
+%         Barlow = nbt_doBarlow(Signal,SignalInfo,0);
+%         nbt_SaveClearObject('Barlow', SignalInfo, SaveDir)
+%         
+%         PeakFit = nbt_doPeakFit(Signal, SignalInfo);
+%         nbt_SaveClearObject('PeakFit', SignalInfo, SaveDir)
+%         
+%         SignalInfo.frequencyRange = [1 45];
+%         
+%         AmpDist_signal = nbt_doAmpDist(Signal, SignalInfo);
+%         nbt_SaveClearObject('AmpDist_signal', SignalInfo, SaveDir)
+
+%% Delta
+%         freqstab_delta=nbt_doFreqStability( Signal, SignalInfo, delta_hp, delta_lp);
+%         nbt_SaveClearObject('freqstab_delta', SignalInfo, SaveDir)
+        
+         [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,delta_hp,delta_lp,delta_fo);
+%         
+%         AmpDist_delta = nbt_doAmpDist(AmplitudeEnvelope, AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpDist_delta', SignalInfo, SaveDir)
+%         
+%         OscB_delta =  nbt_doOscBursts(AmplitudeEnvelope, AmplitudeEnvelopeInfo, 1, 0.95, 0, 0, 0,[],1);
+%         nbt_SaveClearObject('OscB_delta', SignalInfo, SaveDir)
+%         
+%         AmpCorr_delta = nbt_doAmplitudeCorr(AmplitudeEnvelope,AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpCorr_delta', SignalInfo, SaveDir)
+%         
+%         PhaseLock_delta = nbt_doPhaseLocking(Signal,SignalInfo,[delta_hp delta_lp],[0 100],2/delta_hp,[],[],[1 1]);
+%         nbt_SaveClearObject('PhaseLock_delta',SignalInfo, SaveDir)
+        
+         [DFA_delta,DFA_exp] = nbt_doDFA(AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+         nbt_SaveClearObject('DFA_delta', SignalInfo, SaveDir)
+% %% theta frequency
+%         freqstab_theta=nbt_doFreqStability(Signal, SignalInfo, theta_hp, theta_lp);
+%         nbt_SaveClearObject('freqstab_theta', SignalInfo, SaveDir)
+%      
+          [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,theta_hp,theta_lp,theta_fo);
+% %      
+%         AmpDist_theta = nbt_doAmpDist(AmplitudeEnvelope, AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpDist_theta', SignalInfo, SaveDir)
+%      
+%         OscB_theta =  nbt_doOscBursts( AmplitudeEnvelope, AmplitudeEnvelopeInfo, 1, 0.95, 0, 0, 0,[],1);
+%         nbt_SaveClearObject('OscB_theta', SignalInfo, SaveDir)
+%         
+%         AmpCorr_theta = nbt_doAmplitudeCorr(AmplitudeEnvelope,AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpCorr_theta', SignalInfo, SaveDir)
+%         
+%         PhaseLock_theta = nbt_doPhaseLocking(Signal,SignalInfo,[theta_hp theta_lp],[0 100],2/theta_hp,[],[],[1 1]);
+%         nbt_SaveClearObject('PhaseLock_theta',SignalInfo, SaveDir)
+        
+         [DFA_theta,DFA_exp] = nbt_doDFA(AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+        nbt_SaveClearObject('DFA_theta', SignalInfo, SaveDir)
+% %         
+% %% Alpha       
+%         freqstab_alpha=nbt_doFreqStability(Signal, SignalInfo, alpha_hp, alpha_lp);
+%         nbt_SaveClearObject('freqstab_alpha', SignalInfo, SaveDir)
+%         
+         [AmplitudeEnvelope, AmplitudeEnvelopeInfo]= nbt_GetAmplitudeEnvelope(Signal,SignalInfo,alpha_hp,alpha_lp,alpha_fo);
+%         
+%         AmpDist_alpha = nbt_doAmpDist(AmplitudeEnvelope, AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpDist_alpha', SignalInfo, SaveDir)
+%        
+%         OscB_alpha =  nbt_doOscBursts(AmplitudeEnvelope, AmplitudeEnvelopeInfo, 1, 0.95, 0, 0, 0,[],1);
+%         nbt_SaveClearObject('OscB_alpha', SignalInfo, SaveDir)
+%        
+%         AmpCorr_alpha = nbt_doAmplitudeCorr(AmplitudeEnvelope,AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpCorr_alpha', SignalInfo, SaveDir)
+%         
+%         PhaseLock_alpha = nbt_doPhaseLocking(Signal,SignalInfo,[alpha_hp alpha_lp],[0 100],2/alpha_hp,[],[],[1 1]);
+%         nbt_SaveClearObject('PhaseLock_alpha',SignalInfo, SaveDir)
+        
+        [DFA_alpha,DFA_exp] = nbt_doDFA(AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+         nbt_SaveClearObject('DFA_alpha', SignalInfo, SaveDir)
+%         
+% %% Beta frequency
+%         freqstab_beta = nbt_doFreqStability(Signal, SignalInfo, beta_hp, beta_lp);
+%         nbt_SaveClearObject('freqstab_beta', SignalInfo, SaveDir)
+% 
+         [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,beta_hp,beta_lp,beta_fo);
+%         
+%         AmpDist_beta = nbt_doAmpDist(AmplitudeEnvelope, AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpDist_beta', SignalInfo, SaveDir)
+%         
+%         OscB_beta =  nbt_doOscBursts( AmplitudeEnvelope, AmplitudeEnvelopeInfo, 1, 0.95, 0, 0, 0,[],1);
+%         nbt_SaveClearObject('OscB_beta', SignalInfo, SaveDir)
+%         
+%         AmpCorr_beta = nbt_doAmplitudeCorr( AmplitudeEnvelope,AmplitudeEnvelopeInfo);
+%         nbt_SaveClearObject('AmpCorr_beta', SignalInfo, SaveDir)
+%         
+%         PhaseLock_beta = nbt_doPhaseLocking(Signal,SignalInfo,[beta_hp beta_lp],[0 100],2/beta_hp,[],[],[1 1]);
+%         nbt_SaveClearObject('PhaseLock_beta',SignalInfo, SaveDir)
+        
+        [DFA_beta,DFA_exp] = nbt_doDFA( AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+        nbt_SaveClearObject('DFA_beta', SignalInfo, SaveDir,1)  
+        
+        [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,gamma_hp,gamma_lp,gamma_fo);
+         [DFA_gamma,DFA_exp] = nbt_doDFA( AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+        nbt_SaveClearObject('DFA_gamma', SignalInfo, SaveDir,1)  
+        
+        [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,highGamma_hp,highGamma_lp,highGamma_fo);
+         [DFA_highGamma,DFA_exp] = nbt_doDFA( AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+        nbt_SaveClearObject('DFA_highGamma', SignalInfo, SaveDir,1)  
+        
+        [AmplitudeEnvelope, AmplitudeEnvelopeInfo] = nbt_GetAmplitudeEnvelope(Signal,SignalInfo,noharmonicsGamma_hp,noharmonicsGamma_lp,noharmonicsGamma_fo);
+         [DFA_noharmonicsGamma,DFA_exp] = nbt_doDFA( AmplitudeEnvelope, AmplitudeEnvelopeInfo, [DFAshortFit DFAlongFit], [DFAshortCalc DFAlongCalc], DFA_Overlap, DFA_Plot, ChannelToPlot, []);
+        nbt_SaveClearObject('DFA_noharmonicsGamma', SignalInfo, SaveDir,1) 
+        
+        %nbt_importARSQStudentAHN(SignalInfo.file_name,SignalInfo,SaveDir)
+
+    end
+
+NBTfunction_handle = @NBTscript;
+end
