9cb6256dfc1f88c59ae2fd3530d15771670d9ca7
thomasbrockmeier <thomas.brockmeier@gmail.com>
2015-02-11 18:05:34 +0100

    minor

--- fig2.m
+++ fig2.m
@@ -2,17 +2,16 @@ function [ pleasureMat, familiarMat, concentrMat ] = fig2()
 
 files = dir([pwd '\*_analysis.mat']);
 
+segmentIDs = ['BH1'; 'BH2'; 'BR1'; 'BR2'; 'CH1'; 'CH2'; 'GR1'; 'GR2'; ...
+    'HD1'; 'HD2'; 'MZ1'; 'MZ2'];
+
 pleasureMat = zeros(15, 28);    % Matrix(Stimulus, Subject)
 familiarMat = zeros(15, 28);
 concentrMat = zeros(15, 28);
 
 DFA_acousticfeaturesTemplate = load('I:\DFA_acousticfeaturesTemplate.mat');
-musicDFA_list(1:15) = DFA_acousticfeaturesTemplate.DFArray{1:15, 2};
-                                                               % | Rhythm(!)
-                                                 
-% [ musicDFA_list, MDL_Idx ] = sort(musicDFA_list, 'descend');
-MDL_Idx = MDL_Idx + 1;  % +1 lines it up with DFA_acousticfeaturesTemplate
-
+musicDFA_list(1:15) = [DFA_acousticfeaturesTemplate.DFArray{1:15, 2}];
+                                                                % | Rhythm(!)
 subjectIdx = 0;
 for i = 1:length(files)
     % Counters
@@ -32,17 +31,24 @@ end
 pleasureMat([6 12 15], :) = [];
 familiarMat([6 12 15], :) = [];
 concentrMat([6 12 15], :) = [];
-DFA_acousticfeaturesTemplate.DFArray([6 12 15], :) = [];
+musicDFA_list([6 12 15]) = [];
+
+% Sort
+[ musicDFA_list, MDL_Idx ] = sort(musicDFA_list, 'ascend');
 
 % Plot
-f = figure
-s = subplot(1, 4, 1)
-plot([DFA_acousticfeaturesTemplate.DFArray{1:12, 2}], 0.5:1:11.5, '*')
-s = subplot(1, 4, 2)
-boxplot(pleasureMat', 'orientation', 'horizontal')
-s = subplot(1, 4, 3)
-boxplot(familiarMat', 'orientation', 'horizontal')
-s = subplot(1, 4, 4)
-boxplot(concentrMat', 'orientation', 'horizontal')
-
+f = figure;
+s = subplot(1, 4, 1);
+plot(musicDFA_list, 0.5:1:11.5, '*');
+title('Stimulus DFA');
+set(gca, 'YTick', 0.5:1:11.5, 'YTickLabel', segmentIDs(MDL_Idx, :));
+s = subplot(1, 4, 2);
+boxplot(pleasureMat(MDL_Idx, :)', 'orientation', 'horizontal');
+title('Pleasure');
+s = subplot(1, 4, 3);
+boxplot(familiarMat(MDL_Idx, :)', 'orientation', 'horizontal');
+title('Familiarity');
+s = subplot(1, 4, 4);
+boxplot(concentrMat(MDL_Idx, :)', 'orientation', 'horizontal');
+title('Concentration');
 end
