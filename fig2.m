7886bb0831f470e81794ae569c3be0fad1d95d3d
thomasbrockmeier <thomas.brockmeier@gmail.com>
2015-02-12 11:37:27 +0100

    finalize rough fig 2

--- fig2.m
+++ fig2.m
@@ -1,4 +1,4 @@
-function [ pleasureMat, familiarMat, concentrMat ] = fig2()
+function [ pleasureMat, familiarMat, concentrMat ] = fig2(musDim)
 
 files = dir([pwd '\*_analysis.mat']);
 
@@ -10,8 +10,8 @@ familiarMat = zeros(15, 28);
 concentrMat = zeros(15, 28);
 
 DFA_acousticfeaturesTemplate = load('I:\DFA_acousticfeaturesTemplate.mat');
-musicDFA_list(1:15) = [DFA_acousticfeaturesTemplate.DFArray{1:15, 2}];
-                                                                % | Rhythm(!)
+musicDFA_list(1:15) = [DFA_acousticfeaturesTemplate.DFArray{1:15, musDim}];
+
 subjectIdx = 0;
 for i = 1:length(files)
     % Counters
@@ -38,17 +38,41 @@ musicDFA_list([6 12 15]) = [];
 
 % Plot
 f = figure;
-s = subplot(1, 4, 1);
+% s = subplot(1, 4, 1);
+ha = tight_subplot(1, 4, 0, 0.1, 0.1);
+axes(ha(1));
 plot(musicDFA_list, 0.5:1:11.5, '*');
 title('Stimulus DFA');
 set(gca, 'YTick', 0.5:1:11.5, 'YTickLabel', segmentIDs(MDL_Idx, :));
-s = subplot(1, 4, 2);
+% s = subplot(1, 4, 2);
+axes(ha(2));
 boxplot(pleasureMat(MDL_Idx, :)', 'orientation', 'horizontal');
 title('Pleasure');
-s = subplot(1, 4, 3);
+set(gca, 'YTick', [], 'YTickLabel', []);
+% s = subplot(1, 4, 3);
+axes(ha(3));
 boxplot(familiarMat(MDL_Idx, :)', 'orientation', 'horizontal');
 title('Familiarity');
-s = subplot(1, 4, 4);
+set(gca, 'YTick', [], 'YTickLabel', []);
+% s = subplot(1, 4, 4);
+axes(ha(4));
 boxplot(concentrMat(MDL_Idx, :)', 'orientation', 'horizontal');
+set(gca, 'YTick', 1:12, 'YTickLabel', segmentIDs(MDL_Idx, :), 'yaxislocation', 'right');
 title('Concentration');
+
+if musDim == 2
+    musDimID = 'Rhythm';
+elseif musDim == 3
+    musDimID = 'Pitch';
+elseif musDim == 4
+    musDimID = 'Loudness';
+elseif musDim == 5
+    musDimID = 'Average Rhythm, Pitch, and Loudness';
+end
+
+headerString = [ musDimID ' DFA, and Behavioral Responses per Segment'];
+   axes('Position', [0 0 1 1], 'Xlim', [0 1], 'Ylim', [0 1], 'Box', ...
+       'off', 'Visible', 'off', 'Units', 'normalized', 'clipping' , 'off');
+   text(0.5, 1, headerString, 'HorizontalAlignment', 'center', ...
+       'VerticalAlignment', 'top', 'FontWeight', 'bold');
 end
